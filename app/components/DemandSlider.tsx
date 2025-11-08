a886d65722397f7c8f0edba547769913c4a2839e
gmarsenopoulos <gmarsenopoulos@gmail.com>
2025-11-08 21:01:12 +0200

    added slider for demand configuration

---
+++ app/components/DemandSlider.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, useState } from 'react';
+
+import Slider from '@mui/material/Slider';
+import Drawer from '@mui/material/Drawer';
+import CloseIcon from '@mui/icons-material/Close';
+
+const DemandSlider = () => {
+  const [demand, setDemand] = useState(50);
+
+  const valueText = (value: number) => `${value}%`;
+
+  const onSliderChange = (e: Event, value: number) => setDemand(value);
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => setDemand(+e.target.value);
+
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  return (
+    <>
+      <Drawer
+        anchor='top'
+        open={openDrawer}
+        onClose={() => setOpenDrawer(!openDrawer)}
+      >
+        <div className='relative flex gap-10 py-3 px-10 text-black dark:text-white items-center justify-center border-b dark:bg-gray-900  dark:border-gray-600'>
+          <h3 className='inline'>Demand</h3>
+          <Slider
+            onChange={onSliderChange}
+            aria-label="Demand"
+            defaultValue={50}
+            value={demand}
+            getAriaValueText={valueText}
+            valueLabelDisplay="off"
+            shiftStep={10}
+            step={1}
+            marks
+            min={-50}
+            max={100}
+          />
+          <div className='flex items-center p-2 dark:bg-gray-800 border border-gray-950 dark:border-gray-500 rounded-lg'>
+            <input onChange={onInputChange} type="number" name="demand" id="demand" value={demand} min={-50} max={100} className='text-center' />
+          <p>%</p>
+          </div>
+          <CloseIcon onClick={() => setOpenDrawer(!openDrawer)} className='cursor-pointer' />
+        </div>
+      </Drawer>
+      <button onClick={() => setOpenDrawer(!openDrawer)} className='absolute top-0 right-9 p-1 cursor-pointer text-sm bg-white dark:bg-gray-900 border border-t-0 dark:border-gray-600 rounded-b-sm hover:bg-gray-700 transition-all'>Change demand </button>
+    </>
+  )
+}
+
+export default DemandSlider
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+'use client'
 import "./globals.css";
 import { ReactNode } from "react";
+import DemandSlider from "./components/DemandSlider";
 
 interface LayoutProps {
   children: ReactNode
@@ -7,9 +9,12 @@ interface LayoutProps {
 
 const RootLayout = ({ children }:LayoutProps) => {
   return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
+    <html lang="en" >
+      <body className="relative white text-gray-950 dark:bg-gray-950 dark:text-white p-9">
+          <DemandSlider/>
+        <main >
+          {children}
+          </main>
       </body>
     </html>
   )
