569fa601e65daadb258258dad51b4851e2a0966f
gmarsenopoulos <gmarsenopoulos@gmail.com>
2025-11-10 19:31:51 +0200

    adjusted orders table

--- app/components/OrdersTable.tsx
+++ app/components/OrdersTable.tsx
@@ -1,37 +1,60 @@
 'use client'
 
+import { ColumnDef } from "@tanstack/react-table";
+import DataTable from "@/components/ui/data-table";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
+
 const OrdersTable = () => {
-    // const columns: GridColDef[] = [
-    //     {
-    //         field: 'orderDate',
-    //         headerName: 'Order date',
-    //         type: 'date',
-    //         flex:1,
-    //         align:'center',headerAlign:'center',
-    //         valueGetter: (value) => {
-    //             const date = new Date(value);
-    //             return date
-    //         },
-    //         valueFormatter: (value:Date) => {
-    //         if (!value) return '';
-    //         return value.toLocaleDateString('en-GB');
-    //     }
-            
-    //     },
-    //     { field: 'quantity', headerName: 'Quantity', type: 'number',flex:1 ,align:'center',headerAlign:'center',},
-    //     { field: 'unitCost', headerName: 'Unit cost', type: 'number', flex:1,align:'center',headerAlign:'center', valueFormatter:(value)=>`${value}€` },
-    //     { field: 'totalCost', headerName: 'Total cost', type: 'number',flex:1,align:'center',headerAlign:'center', valueFormatter:(value)=>`${value}€` },
-    //     { field: 'status', headerName: 'Status', type: 'string', flex:1,align:'center',headerAlign:'center',renderCell:(params)=>{
-    //         const statusColors={
-    //             planned:'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200',
-    //             confirmed:'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200',
-    //             delivered:'bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200'
-    //         }
 
-    //         return <span className={`px-2 py-1 font-semibold border rounded-md ${statusColors[params.value as keyof typeof statusColors]}`}>{params.value}</span>
-    //     } },
+    const columns: ColumnDef<any>[] = [
+        {
+            accessorKey: 'orderDate',
+            cell: ({ row }) => {
+                if (!row.getValue('orderDate')) return '';
+                const date = new Date(row.getValue('orderDate'));
+                return date.toLocaleDateString('en-GB');
+            },
+            header:({column})=>(
+                <div className="flex items-center gap-2"><span>Order date</span> <Button variant='ghost' onClick={()=>column.toggleSorting(column.getIsSorted()==="asc")} className="cursor-pointer"><ArrowUpDown className="h-4 w-4" /></Button></div>
+                
+            )
+        },
+        {
+            accessorKey: 'quantity',
+            header: 'Quantity'
+        },
+        {
+            accessorKey: 'unitCost',
+            cell:({row})=>`${row.getValue('unitCost')}€`,
+            header:({column})=>(
+                <div className="flex items-center gap-2"><span>Unit cost</span> <Button variant='ghost' onClick={()=>column.toggleSorting(column.getIsSorted()==="asc")} className="cursor-pointer"><ArrowUpDown className="h-4 w-4" /></Button></div>
+            )
+        },
+        {
+            accessorKey: 'totalCost',
+            cell:({row})=>`${row.getValue('totalCost')}€`,
+            header:({column})=>(
+                <div className="flex items-center gap-2"><span>Total cost</span> <Button variant='ghost' onClick={()=>column.toggleSorting(column.getIsSorted()==="asc")} className="cursor-pointer"><ArrowUpDown className="h-4 w-4" /></Button></div>
+            )
+        },
+        {
+            accessorKey: 'status',
+            cell: ({ row }) => {
+                const status = row.getValue('status') as string;
+                const statusColors = {
+                    planned: 'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200',
+                    confirmed: 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200',
+                    delivered: 'bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200'
+                }
 
-    // ];
+                return <span className={`px-2 py-1 font-semibold border rounded-md ${statusColors[status as keyof typeof statusColors]}`}>{status}</span>
+            },
+            header:({column})=>(
+                <div className="flex items-center gap-2"><span>Status</span> <Button variant='ghost' onClick={()=>column.toggleSorting(column.getIsSorted()==="asc")} className="cursor-pointer"><ArrowUpDown className="h-4 w-4" /></Button></div>
+            )
+        },
+    ]
 
     const rows = [
         {
@@ -120,21 +143,8 @@ const OrdersTable = () => {
         }
     ];
 
-    const paginationModel = { page: 0, pageSize: 10 };
     return (
-        <></>
-            // <DataGrid
-            //     rows={rows}
-            //     columns={columns}
-            //     initialState={{ pagination: { paginationModel } }}
-            //     pageSizeOptions={[5, 10,15,20]}
-            //     sx={{ border: 1,width:'100%' }}
-            //     disableColumnResize
-            //     disableColumnFilter
-            //     disableAutosize
-            //     disableColumnMenu
-            //     disableRowSelectionOnClick
-            // />
+        <DataTable columns={columns} data={rows} />
     )
 }
 
